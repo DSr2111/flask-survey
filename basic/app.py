5ba4d12669584237e99a3491c5d904686c7664a4
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-11 23:18:32 -0500

    begin and answer routes code

--- basic/app.py
+++ basic/app.py
@@ -1,25 +1,41 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
 
+RESPONSES_KEY = "responses"
 app = Flask(__name__)
 
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = "never-tell"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 
 debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def show_survey_start():
     """Pick survey to begin."""
+    return render_template("survey_start.html", survey=survey)
 
 
 
-
-@app.route("/")
+@app.route("/begin", methods=["POST"])
 def start_survey():
     """Begin survey."""
+    session[RESPONSES_KEY] = []
+
+    return redirect("/questions/0")
 
 
-@app.route("/")
+@app.route("/answer", methods=["POST"])
 def handle_question():
     """Save responses and continue to next question."""
+    choice = request.form['answer']
+
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses)== len(survey.questions)):
+        return redirect("/complete")
+    
+    
