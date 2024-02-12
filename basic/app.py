ebb219c6e11af0083cd0cc1d68eef4f45b2d9e06
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-11 23:25:27 -0500

    dealing with questions logic

--- basic/app.py
+++ basic/app.py
@@ -38,4 +38,24 @@ def handle_question():
     if (len(responses)== len(survey.questions)):
         return redirect("/complete")
     
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    """Shows the current questions"""
+    responses = session.get(RESPONSES_KEY)
+
+    if (responses is None):
+        return redirect ("/")
     
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    if (len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template(
+        "question.html", question_num=qid, question=question)
